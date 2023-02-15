1bcbf76592bf26394078f09d7b4f71812bcd5874
Sajid Haniff <sajid.haniff@gmail.com>
2023-02-14 19:41:38 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -15,49 +15,13 @@ let p = (sk) => {
         canvas.id('canvas');
         sk.background(40);
 
-        rosette = rosetteBuilder(5, sk);
+        rosette = rosetteBuilder(15, sk);
     }
 
     sk.draw = () =>{
 
-        /*onst width = 400;
-        const height = 400;
-
-        let win = {
-            left:   -10,
-            right:   10,
-            top:     10,
-            bottom: -10
-        }
-
-        let viewport = {
-            left:   0,
-            right:   1,
-            top:    1,
-            bottom: 0
-        }
-
-        let tmp1 = (win.right - win.left);
-        let tmp2 = (win.top - win.bottom);
-
-        let sx = (viewport.right - viewport.left) / tmp1;
-        let sy = (viewport.top - viewport.bottom) / tmp2;
-
-        let tx = (viewport.left * win.right - viewport.right * win.left) / tmp1;
-        let ty = (viewport.bottom * win.top - viewport.top * win.bottom) / tmp2;
-
-        sk.drawingContext.transform(1, 0, 0, -1, 0, height);
-        sk.drawingContext.transform(width, 0, 0, height,0, 0);
-        sk.drawingContext.transform(sx, 0, 0, sy, tx, ty);
-
-         */
-
         rosette.draw(sk);
     }
-
-
-
-
 }
 
 const P5 = new p5(p);
--- src/rosette_builder.js
+++ src/rosette_builder.js
@@ -20,11 +20,6 @@ export const rosetteBuilder = (NUM_VERTEX, sk) => {
     let height = 400;
     const {sx, sy, tx, ty} = createGraphicsContext(win, view, width, height);
 
-    //sk.drawingContext.transform(1, 0, 0, -1, 0, height);
-    //sk.drawingContext.transform(width, 0, 0, height,0, 0);
-    //sk.drawingContext.transform(sx, 0, 0, sy, tx, ty);
-    //sk.push();
-
     (() => {
         for (let i = 0; i < NUM_VERTEX; i++) {
             X[i] = radius * Math.cos(theta);
@@ -33,24 +28,8 @@ export const rosetteBuilder = (NUM_VERTEX, sk) => {
         }
     })();
 
-    //const radian = glMatrix.toRadian(360);
-    //const v2 = vec2.fromValues(2,3);
-    //  const v3 = vec2.fromValues(2,3,4);
-
-
-    //const radian = glMatrix.toRadian(360);
-    //const v2 = vec2.fromValues(2,3);
-
-
-
-    //console.log("testing " + vec2.str(v2));
-    //console.log("testing " + v2[1]);
-
     return {
         draw: (sk) => {
-
-            //sk.pop();
-
 
             sk.drawingContext.transform(1, 0, 0, -1, 0, height);
             sk.drawingContext.transform(width, 0, 0, height,0, 0);
@@ -75,7 +54,6 @@ export const rosetteBuilder = (NUM_VERTEX, sk) => {
             for (let i = 0; i < 18; i++) {
                 sk.circle(X[i], Y[i], 0.2);
             }
-            //sk.push();
         }
     }
 }
